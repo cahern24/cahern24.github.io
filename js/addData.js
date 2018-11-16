be0334cb15e602080d020116a262d13cac093b83
cahern24 <44711120+cahern24@users.noreply.github.com>
2018-11-16 10:08:44 -0800

    fixed send button nigs

--- js/addData.js
+++ js/addData.js
@@ -2,17 +2,13 @@ const form = document.querySelector('#add-user-input');
 form.addEventListener('submit', function(evt) {
 evt.preventDefault();
 db.collection('userInput').add({
-name: form.name.value,
+firstName: form.firstName.value,
+lastName: form.lastName.value,
 email: form.email.value,
 message: form.message.value
 });
-form.name.value = '';
+form.firstName.value = '';
+form.lastName.value = '';
 form.email.value = '';
 form.message.value = '';
-})
-db.collection('userInput').add({
-createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-name: form.name.value,
-email: form.email.value,
-message: form.message.value
 });
